47ca764a58fa9283c25081b95fd38439d799c4c6
harshhariyani404 <harsh.hariyani404@gmail.com>
2025-09-22 10:19:12 +0530

    SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Herosec from './Herosec'
-
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Herosec from './Herosec';
+import { Helmet } from 'react-helmet-async';   // 👈 Add this
 
 const App = () => {
   return (
     <BrowserRouter>
-        <Header />
-        <Herosec />
-        <Footer />
-      </BrowserRouter>
-  )
-}
+      {/* Global meta tags */}
+      <Helmet>
+        <title>Harsh Portfolio</title>
+        <meta
+          name="description"
+          content="React portfolio showcasing projects and skills."
+        />
+      </Helmet>
 
-export default App
+      <Header />
+      <Herosec />
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
